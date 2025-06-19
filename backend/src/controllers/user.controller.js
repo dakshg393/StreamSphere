a21cd0d1e2c911940ae1c29b953d14da934aae74
dakshg393 <142725301+dakshg393@users.noreply.github.com>
2025-06-19 13:39:11 +0530

    Update user controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -119,7 +119,8 @@ const loginUser = asyncHandler(async (req, res) => {
 
     const option = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite:"None"
     }
 
     return res
@@ -155,7 +156,8 @@ const logoutUser =  asyncHandler(async(req,res) => {
 
     const option = {
         httpOnly: true,
-        secure: true
+        secure: true,
+        sameSite:"None"
     }
 
     return res
@@ -189,7 +191,8 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
     
         const options = {
             httpOnly:true,
-            secure:true
+            secure:true,
+            sameSite:"None"
         }
     
         const {accessToken,newRefreshToken} =await genertaeAccesssAndRefreshToken(user._id)
