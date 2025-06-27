85d227c6ad4176f68d59d5dc93cd5c0393cb60af
dakshg393 <142725301+dakshg393@users.noreply.github.com>
2025-06-27 23:57:19 +0530

    Update store fix bug of user persitent

--- frontend/src/Store/user.Store.js
+++ frontend/src/Store/user.Store.js
@@ -1,23 +1,34 @@
 
-import { create } from 'zustand';
-import { persist } from 'zustand/middleware';
+// import { create } from 'zustand';
+// import { persist } from 'zustand/middleware';
 
-const useUserStore = create(
-  persist(
-    (set) => ({
-      user: null,
-      setUser: (user) => set({ user }),
-      logout: () => set({ user: null }),
-    }),
-    {
-      name: 'user-session-storage',
-      storage: {
-        getItem: (key) => sessionStorage.getItem(key),
-        setItem: (key, value) => sessionStorage.setItem(key, value),
-        removeItem: (key) => sessionStorage.removeItem(key),
-      },
-    }
-  )
-);
+// const useUserStore = create(
+//   persist(
+//     (set) => ({
+//       user: null,
+//       setUser: (user) => set({ user }),
+//       logout: () => set({ user: null }),
+//     }),
+//     {
+//       name: 'user-session-storage',
+//       storage: {
+//         getItem: (key) => sessionStorage.getItem(key),
+//         setItem: (key, value) => sessionStorage.setItem(key, value),
+//         removeItem: (key) => sessionStorage.removeItem(key),
+//       },
+//     }
+//   )
+// );
+
+// export default useUserStore;
+
+// src/store/useUserStore.js
+import { create } from 'zustand';
+
+const useUserStore = create((set) => ({
+  user: null,
+  setUser: (user) => set({ user }),
+  logout: () => set({ user: null }),
+}));
 
 export default useUserStore;
